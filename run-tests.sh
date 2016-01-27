6620adacfc1e48915b79e6a7a89c96fd6f647f1d
Jens Schaa <jens.schaa@otto.de>
2016-01-27 14:43:06 +0100

    Uses which-python for tests. Do not autoinstall venv

--- run-tests.sh
+++ run-tests.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
-
-if [ ! -d "venv" ]; then
-    ./setup.sh
-fi
+set -e
+source which-python.sh
 
 if [ -z "${1}" ]; then
     environment="test"
@@ -10,13 +8,13 @@ else
     environment="${1}"
 fi
 
-./venv/local/bin/python -m tests.test_api $environment
+${PYTHON} -m tests.test_api $environment
 exit_codes=$?
 
-./venv/local/bin/python -m tests.test_business_rules $environment
+${PYTHON} -m tests.test_business_rules $environment
 exit_codes=$(( $exit_codes + $? ))
 
-./venv/local/bin/python -m tests.test_validate_tickets $environment
+${PYTHON} -m tests.test_validate_tickets $environment
 exit_codes=$(( $exit_codes + $? ))
 
 if [ $exit_codes -eq 0 ]; then
