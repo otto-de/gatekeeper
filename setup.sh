c8c7fdbf1662d93237e36fe83d7fa70bb91b94a1
Jens Schaa <jens.schaa@otto.de>
2016-02-04 13:07:17 +0100

    adds options to setup script to include options whether or not to install venv and include additional dependencies

--- setup.sh
+++ setup.sh
@@ -1,22 +1,69 @@
 #!/usr/bin/env bash
+
+VENV=true
+DOCS_DEPS=false
+
+usage() { echo "Usage: $0 [--no-venv] [--doc-deps]" 1>&2;
+	  echo "Options:"
+	  echo "	--no-venv   Install packages globally. May need root privileges."
+	  echo "	--doc-deps	Includes dependencies to generate docs (ie. Sphinx)."
+          exit 1; }
+
+while getopts "h-:" opt; do
+  case $opt in
+    -)
+      case $OPTARG in
+        no-venv)
+            echo "venv disabled."
+            VENV=false
+            ;;
+        doc-deps)
+            echo "Includes dependencies to generate docs"
+            DOCS_DEPS=true
+            ;;
+        *)
+            echo "Invalid option: $OPTARG" >&2
+            usage
+            ;;
+      esac
+      ;;
+    h)
+      usage
+      ;;
+    *)
+      echo "Invalid option: -$OPTARG" >&2
+      usage
+      ;;
+  esac
+done
+
+useVirtualEnv() {
+  echo "Using VirtualEnv"
+  python -m pip install --user virtualenv
+  python -m virtualenv venv
+  source venv/bin/activate
+}
+
 if [ ! -f "get-pip.py" ]; then
     ./get-pip.sh
 fi
+
 python get-pip.py --user
 
-python -m pip install --user virtualenv
-python -m virtualenv venv
-
-source venv/bin/activate
+if $VENV; then
+  useVirtualEnv
+else
+  echo "Installing packages globally"
+fi
 
 python -m pip install pymongo flask datetime tzlocal python-dateutil humanize babel
 
-python -m pip install Sphinx sphinxcontrib-httpdomain
+if $DOCS_DEPS; then
+    python -m pip install Sphinx sphinxcontrib-httpdomain
+fi
 
 git clone -c http.sslVerify=false https://github.com/myusuf3/delorean.git
 cd delorean
 python setup.py install
 cd ..
 rm -rf delorean
-
-deactivate
