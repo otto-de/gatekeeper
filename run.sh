c23d6db3d6a5346a26dfd8c11ee060b0f85c306d
Jens Schaa <jens.schaa@otto.de>
2016-02-04 11:42:55 +0100

    improved help message for run.sh parameters

--- run.sh
+++ run.sh
@@ -5,11 +5,11 @@ DEBUG=false
 PORT=8080
 ENVIRONMENT="local"
 
-usage() { echo "Usage: $0 [-p <port>] [-e <environment>] [-v <verbose>]" 1>&2;
+usage() { echo "Usage: $0 [-p <port>] [-e <environment>] [-v]" 1>&2;
 	  echo "Options:"
-	  echo "	-p	Port: Specify port. Default is ${PORT}."
-	  echo "	-e	Environment: Specify which config to load. Default is ${ENVIRONMENT}."
-	  echo "	-v	Enables debug mode."
+	  echo "	-p <Port>           Specify port. Default is ${PORT}."
+	  echo "	-e <Environment>    Specify which config to load. Default is ${ENVIRONMENT}."
+	  echo "	-v                  Enables debug mode."
           exit 1; }
 
 while getopts "e:p:vh" opt; do
