dee5383b46d334d503788790bd22ca46bf9a47bf
Jens Schaa <jens.schaa@otto.de>
2016-02-10 09:54:15 +0100

    updates run script. No need for two seperate run.py anymore

--- run.py
+++ run.py
@@ -1,6 +1,16 @@
-#!./venv/local/bin python
 import sys
 from app.app import create_app
 
-app = create_app(environment=sys.argv[1])
-app.run(debug=True, use_reloader=False, port=int(sys.argv[2]))
+environment = sys.argv[1]
+port = int(sys.argv[2])
+debug = sys.argv[3] == "true"
+
+app = create_app(environment=environment)
+print("\nApplication staring...")
+print(" Environment: " + str(environment))
+print(" Port: " + str(port))
+print(" Debug: " + str(debug))
+if debug:
+    app.run(debug=True, use_reloader=False, port=port)
+else:
+    app.run(debug=False, use_reloader=False, port=port, host='0.0.0.0')
--- run.sh
+++ run.sh
@@ -5,11 +5,11 @@ DEBUG=false
 PORT=8080
 ENVIRONMENT="local"
 
-usage() { echo "Usage: $0 [-p <port>] [-e <environment>] [-v]" 1>&2;
+usage() { echo "Usage: $0 [-p <port>] [-e <environment>] [-v <verbose>]" 1>&2;
 	  echo "Options:"
-	  echo "	-p <Port>           Specify port. Default is ${PORT}."
-	  echo "	-e <Environment>    Specify which config to load. Default is ${ENVIRONMENT}."
-	  echo "	-v                  Enables debug mode."
+	  echo "	-p <port> 	Port: Specify port. Default is ${PORT}."
+	  echo "	-e <env>	Environment: Specify which config to load. Default is ${ENVIRONMENT}."
+	  echo "	-v	        Enables debug mode."
           exit 1; }
 
 while getopts "e:p:vh" opt; do
@@ -38,12 +38,4 @@ done
 
 source which-python.sh
 
-if $DEBUG; then
-    echo "Running debug"
-    RUN=run.py
-else
-    echo "Running productive"
-    RUN=runp.py
-fi
-
-${PYTHON} ${RUN} ${ENVIRONMENT} $PORT
+${PYTHON} run.py ${ENVIRONMENT} $PORT $DEBUG
