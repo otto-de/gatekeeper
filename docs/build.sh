5e84d439f206241731f583f01d8c1c2df4f69c09
Jens Schaa <jens.schaa@otto.de>
2016-02-10 18:10:19 +0100

    Adds error message if sphinx was not found

--- docs/build.sh
+++ docs/build.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 cd "$(dirname "$0")"
-make html SPHINXBUILD='../venv/bin/sphinx-build'
+
+SPHINX=../venv/bin/sphinx-build
+if [ ! -f ${SPHINX} ]; then
+    echo -e "\e[1m\e[31mCould not find sphinx to generate docs. Please run 'setup.sh --doc-deps'.\e[0m"
+    exit 1
+fi
+
+make html SPHINXBUILD="${SPHINX}"
