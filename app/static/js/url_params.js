3a5baf032ccb75942a09973f44e05c0e75044dc8
Jens Schaa <jens.schaa@otto.de>
2016-02-18 09:27:15 +0100

    js| adds tab switch and some reformating

--- app/static/js/url_params.js
+++ app/static/js/url_params.js
@@ -1,42 +1,85 @@
-function handleParams(key, value, single_value) {
-    key = encodeURI(key);
-    value = encodeURI(value);
-    var term_list = document.location.search.substr(1).split('&');
+window.o_p13n = window.o_p13n || {};
+window.o_p13n.tools = window.o_p13n.tools || {};
 
-    var params = {}
-    if(term_list != ""){
-        for (i in term_list) {
-            var pair = term_list[i].split('=')
-            params[pair[0]] = pair[1].split(',')
+o_p13n.tools.url_params = function () {
+    "use strict";
+    var module = {};
+
+    module.currentLocation = {
+        getSearch: function () {
+            return window.location.search;
+        },
+        setSearch: function (search) {
+            window.location.search = search;
+        },
+        setState: function (url) {
+            window.history.pushState({}, "", window.location.pathname + "" + url);
         }
-    }
+    };
 
-    if(params[key]){
-        index = params[key].indexOf(value);
-        if (index > -1) {
-             params[key].splice(index, 1);
-        } else {
-            if(single_value){
-                params[key] = value
-            } else {
-                params[key].push(value)
+    var setUrlParameter = function (key, value, reload) {
+        key = encodeURI(key);
+        value = encodeURI(value);
+
+        // get old params
+        var term_list = module.currentLocation.getSearch().substr(1).split('&');
+
+        var params = {};
+        if (term_list != "") {
+            for (var i in term_list) {
+                var pair = term_list[i].split('=');
+                if (pair.length === 2) {
+                    params[pair[0]] = pair[1].split(',')
+                }
             }
         }
-    } else {
-        params[key] = value
-    }
 
-    new_search = []
-    for(key in params){
-        if(params[key].length != 0){
-            console.log("params[key]", params[key])
-            var val = params[key]
-            new_search.push([key, val].join('='))
+        params[key] = value;
+
+        var new_search = [];
+        for (var param in  params) {
+            if (params[param].length != 0) {
+                var val = params[param];
+                new_search.push([param, val].join('='))
+            }
         }
-    }
-    document.location.search = new_search.join('&');
-}
+        new_search = new_search.join('&');
 
-document.getElementById('style_change').onclick = function() {
-    handleParams("style", "night", true);
+        if (reload) {
+            module.currentLocation.setSearch(new_search);
+        } else {
+            new_search = new_search ? "?" + new_search : "";
+            module.currentLocation.setState(new_search);
+        }
+    };
+
+    var manageStyleChange = function () {
+        $('#style_change').click(function () {
+                setUrlParameter("style", $(this).data("style"), true);
+            }
+        );
+    };
+
+    var changeUrlOnTabClick = function () {
+        $('.tab-title').each(function () {
+            var $this = $(this);
+            var group = $this.data("group");
+            $this.click(function () {
+                setUrlParameter("tab", group, false);
+            });
+        });
+    };
+
+    module.init = function () {
+        manageStyleChange();
+        changeUrlOnTabClick();
+    };
+
+    return module
+
 };
+
+$(window).load(function () {
+    o_p13n.tools.url_params().init();
+});
+
