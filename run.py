cf162c1e0c629883b009e0e6d327e08e8fd5f33d
Jens Schaa <jens.schaa@otto.de>
2016-04-28 22:11:56 +0200

    Debug mode should not change network settings

--- run.py
+++ run.py
@@ -10,7 +10,4 @@ print("\nApplication staring...")
 print(" Environment: " + str(environment))
 print(" Port: " + str(port))
 print(" Debug: " + str(debug))
-if debug:
-    app.run(debug=True, use_reloader=False, port=port)
-else:
-    app.run(debug=False, use_reloader=False, port=port, host='0.0.0.0')
+app.run(debug=debug, use_reloader=False, port=port, host='0.0.0.0')
