48243e42bf5dd14abdf2363616493d41c482aa5c
Eduard Weigandt <eduard.weigandt@otto.de>
2017-07-10 15:51:08 +0200

    manage sse connections

--- backend/src/routes/sse.js
+++ backend/src/routes/sse.js
@@ -1,20 +1,27 @@
 const router = require('express').Router();
 const sse = require('../helper/sse-client');
 const gateService = require('../services/gateService');
+const uuidv4 = require('uuid/v4');
 
-const connections = [];
+let connections = [];
 
 router.get('/', async (req, res) => {
     sse(req, res);
-    connections.push(res);
+    const id = uuidv4();
+    connections.push({id, res});
+    console.log('new connection: ' + id);
     res.sse.sendEvent('state', await gateService.getAllGates());
-    res.sse.addListener('close', ()=> console.log("everything is closed -_+"))
+    req.connection.on('close', () => {
+        console.log('remove closed connection: ' + id);
+        connections = connections.filter((con) => con.id !== id)
+    });
 });
 
 async function notifyStateChange() {
     const gates = await gateService.getAllGates();
-    connections.forEach(res => {
-        res.sse.sendEvent('state', gates)
+    connections.forEach(con => {
+        console.log('notify connection: ' + con.id);
+        con.res.sse.sendEvent('state', gates)
     });
 }
 
